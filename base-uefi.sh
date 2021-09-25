e0a90a57a188dca7ddd428a783c69aa08caeba2e
dutofanim <etofanim@gmail.com>
2021-09-25 14:08:26 -0300

    Modified base, gnome and kde scripts.

--- base-uefi.sh
+++ base-uefi.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo -e "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo -e "KEYMAP=br-abnt2" >> /etc/vconsole.conf
+echo -e "archGDM" >> /etc/hostname
+echo -e "127.0.0.1\tlocalhost \n::1 \t\tlocalhost \n127.0.1.1\tarchGDM.localdomain\tarchGDM" >> /etc/hosts
 echo root:password | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub grub-btrfs efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font udisks2 zip p7zip
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# pacman -S --noconfirm xf86-video-amdgpu # If you have an AMDGPU 
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
@@ -35,11 +33,11 @@ systemctl enable libvirtd
 systemctl enable firewalld
 systemctl enable acpid
 
-useradd -m ermanno
-echo ermanno:password | chpasswd
-usermod -aG libvirt ermanno
+useradd -m du
+echo du:password | chpasswd
+usermod -aG libvirt du
 
-echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+echo "du ALL=(ALL) ALL" >> /etc/sudoers.d/du
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
--- kde.sh
+++ kde.sh
@@ -3,28 +3,24 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Sy
 
 sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 sudo firewall-cmd --add-port=1025-65535/udp --permanent
 sudo firewall-cmd --reload
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
+git clone https://github.com/Morganamilo/paru.git $HOME/Downloads
+cd $HOME/Downloads/paru/
 makepkg -si --noconfirm
-
-#pikaur -S --noconfirm system76-power
-#sudo systemctl enable --now system76-power
-#sudo system76-power graphics integrated
-#pikaur -S --noconfirm auto-cpufreq
-#sudo systemctl enable --now auto-cpufreq
-
+cd .. && rm -rf paru
 
 sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder obs-studio vlc papirus-icon-theme kdenlive materia-kde
 
-sudo flatpak install -y spotify
-
 sudo systemctl enable sddm
+sudo systemctl enable --now system76-power
+sudo system76-power graphics integrated
+sudo systemctl enable --now auto-cpufreq
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
